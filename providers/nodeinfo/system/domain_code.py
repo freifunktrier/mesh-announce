6d44f3229440e3cbd005b44f99361361cc580124
genofire <geno+dev@fireorbit.de>
2020-02-05 16:12:07 +0100

    disable cache for domain_code

--- providers/nodeinfo/system/domain_code.py
+++ providers/nodeinfo/system/domain_code.py
@@ -4,6 +4,9 @@ import socket
 domains = { 'bat0':'fftr_GW', 'bat01':'fftr_GW', 'bat02':'fftr_GW', 'bat03':'fftr_GW', 'bat04':'fftr_GW', 'bat05':'fftr_GW'}
 
 class Source(providers.DataSource):
+    def cache_ttl(self):
+        return 0
+    
     def required_args(self):
         return ['batadv_dev']
 
