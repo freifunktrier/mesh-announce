6222b6e7c4fe21737299ddf2419a45884e1f6412
tackin <tackin@mail2.digital-layout.de>
2020-04-04 19:11:25 +0200

    add alternative domain coding

--- providers/nodeinfo/system/domain_code.py
+++ providers/nodeinfo/system/domain_code.py
@@ -6,3 +6,18 @@ class Source(providers.DataSource):
     def call(self, domain_code):
         return domain_code
 
+# ALTERNATIVE
+#import socket
+
+#domains = { 'bat0':'fftr_GW', 'bat01':'fftr_GW', 'bat02':'fftr_GW', 'bat03':'fftr_GW', 'bat04':'fftr_GW', 'bat05':'fftr_GW'}
+
+#class Source(providers.DataSource):
+#    def cache_ttl(self):
+#        return 0
+#    
+#    def required_args(self):
+#        return ['batadv_dev']
+#
+#    def call(self, batadv_dev):
+#        return domains[batadv_dev]
+
